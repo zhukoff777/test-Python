6877740b0ef659f68f5fda5164dc643c05686c1b
zhukoff777 <55098988+zhukoff777@users.noreply.github.com>
2022-06-04 23:52:51 +0300

    First lesson practice

---
+++ test1.py
@@ -0,0 +1,25 @@
+#print (3 ** 30)
+# length of string
+#print (len(str(3 ** 40)))
+
+# 
+# import random
+# random.random
+# x = random.choice([4, 5, 3, 66, 43 ])
+# print (x)
+
+# rabota so strokami
+# spisok ob'edinenie
+#  list ()  .join()
+# 
+# bytearray(b'  )   .expand (b'  )   .decode()
+
+# metods
+#  .find ()   .replace ()
+#  
+#  .upper ()  .isalpha ()  .isdigit ()  .split ()  .rstrip ()
+
+#  Spravka
+#  dir ()   help ()
+#   Upravliauschie simvoly
+# \n   \t   \0
